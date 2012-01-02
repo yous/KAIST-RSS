e86751c0202c4080a9e89cccefa4dcc09b962fcf
yous <chacha@kaist.ac.kr>
2012-01-02 23:54:32 +0900

    RSS 데이터 가져오는 코드와 WEBrick으로 출력하는 코드 분리

---
+++ aradata.rb
@@ -0,0 +1,38 @@
+require "rss/maker"
+require "mechanize"
+
+class ARA_RSS
+  attr_accessor :rss
+
+  def initialize(version = "2.0")
+    @version = version
+    get
+  end
+
+  def data
+    get.to_xml
+  end
+
+  private
+  def get
+    @rss = RSS::Maker.make(@version) do |m|
+      m.channel.title = "ARA Wanted RSS Feed"
+      m.channel.link = "http://ara.kaist.ac.kr/board/Wanted/"
+      m.channel.description = "News at ARA Wanted Board"
+      m.items.do_sort = true
+
+      a = Mechanize.new
+      resp = a.get "http://ara.kaist.ac.kr/board/Wanted/"
+      resp.search('//table[@class="articleList"]/tbody/tr').each do |r|
+        m.items.new_item do |item|
+          title = (r.at('td[@class="title "]') or r.at('td[@class="title  deleted"]'))
+          item.title = title.inner_text.strip
+          item.date = Time.parse(r.at('td[@class="date"]').inner_html.strip)
+          item.guid.content = r.at('td[@class="articleid hidden"]').inner_text.strip
+          item.guid.isPermaLink = false
+          item.link = "http://ara.kaist.ac.kr/board/Wanted/#{item.guid.content}"
+        end
+      end
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,10 +1,8 @@
-require "mechanize"
 require "webrick"
-require "rss/maker"
-
-a = Mechanize.new
+require "./aradata.rb"
 
 server = WEBrick::HTTPServer.new :Port => (ARGV[0] or 8888).to_i
+
 server.mount_proc("/") do |req, res|
   res["Last-Modified"] = Time.now
   res["Cache-Control"] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
@@ -14,27 +12,8 @@ server.mount_proc("/") do |req, res|
   res.status = 200
   res["Content-Type"] = "text/xml"
 
-  version = "2.0"
-  content = RSS::Maker.make(version) do |m|
-    m.channel.title = "ARA Wanted RSS Feed"
-    m.channel.link = "http://ara.kaist.ac.kr/board/Wanted/"
-    m.channel.description = "News at ARA Wanted Board"
-    m.items.do_sort = true
-
-    resp = a.get "http://ara.kaist.ac.kr/board/Wanted/"
-    resp.search('//table[@class="articleList"]/tbody/tr').each do |r|
-      m.items.new_item do |item|
-        title = (r.at('td[@class="title "]') or r.at('td[@class="title  deleted"]'))
-        item.title = title.inner_text.strip
-        item.date = Time.parse(r.at('td[@class="date"]').inner_html.strip)
-        item.guid.content = r.at('td[@class="articleid hidden"]').inner_text.strip
-        item.guid.isPermaLink = false
-        item.link = "http://ara.kaist.ac.kr/board/Wanted/#{item.guid.content}"
-      end
-    end
-  end
-
-  res.body = content.to_xml
+  rss = ARA_RSS.new
+  res.body = rss.data
 end
 
 trap("INT") { server.shutdown }
