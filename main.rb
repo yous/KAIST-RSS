f3fdad2ccc4078a28a92bef1f782203ddd6c8423
yous <chacha@kaist.ac.kr>
2012-01-02 14:41:52 +0900

    Address, Port 설정

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require "rss/maker"
 
 a = Mechanize.new
 
-server = WEBrick::HTTPServer.new :BindAddress => "127.0.0.1", :Port => (ARGV[0] or 8080).to_i
+server = WEBrick::HTTPServer.new :BindAddress => "192.168.80.140", :Port => (ARGV[0] or 8888).to_i
 server.mount_proc("/") do |req, res|
   res["Last-Modified"] = Time.now
   res["Cache-Control"] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
@@ -14,15 +14,14 @@ server.mount_proc("/") do |req, res|
   res.status = 200
   res["Content-Type"] = "text/xml"
 
-  resp = a.get "http://ara.kaist.ac.kr/board/Wanted/"
   version = "2.0"
-
   content = RSS::Maker.make(version) do |m|
     m.channel.title = "ARA Wanted RSS Feed"
     m.channel.link = "http://ara.kaist.ac.kr/board/Wanted/"
     m.channel.description = "News at ARA Wanted Board"
     m.items.do_sort = true
 
+    resp = a.get "http://ara.kaist.ac.kr/board/Wanted/"
     resp.search('//table[@class="articleList"]/tbody/tr').each {|r|
       i = m.items.new_item
       i.title = r.search('td[@class="title "]')[0].inner_html.strip
