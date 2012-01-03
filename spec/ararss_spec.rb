b9421a20b1e259ac15243dc8a9944d15ed96a789
yous <yousbe@gmail.com>
2012-01-04 00:23:11 +0900

    spec의 테스트를 deleted article이 있는 case로 구체화

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -3,11 +3,11 @@ require "fakeweb"
 require "./lib/ararss"
 
 describe "ARA RSS" do
-  it "gets the first page of ARA board and parse" do
-    stream = File.read("./spec/pages/fake_ara_page.html")
+  it "gets the page that contains deleted articles and parse" do
+    stream = File.read("./spec/pages/page_deleted.html")
     FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
     
     rss = ARA_RSS.new
-    rss.data.should eq(File.read("./spec/pages/fake_ara_page.xml"))
+    rss.data.should eq(File.read("./spec/pages/page_deleted.xml"))
   end
 end
