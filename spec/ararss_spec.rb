fb5bd2ed5f8c630c5b7e0ed4b8432854eefcc05c
yous <yousbe@gmail.com>
2012-01-04 02:54:08 +0900

    테스트 설명 보기좋게 context 추가

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -3,13 +3,15 @@ require "fakeweb"
 require "./lib/ararss"
 
 describe "ARA RSS" do
-  ["deleted", "one", "one_deleted", "none", "none_2", "trouble"].each do |attr|
-    it "gets the page that contains #{attr} article(s) and parse" do
-      stream = File.read("./spec/pages/page_#{attr}.html")
-      FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
+  context "gets the page that contains" do
+    ["deleted", "one", "one_deleted", "none", "none_2", "trouble"].each do |attr|
+      it "#{attr} article(s) and parse" do
+        stream = File.read("./spec/pages/page_#{attr}.html")
+        FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
 
-      rss = ARA_RSS.new
-      rss.data.should eq(File.read("./spec/pages/page_#{attr}.xml"))
+        rss = ARA_RSS.new
+        rss.data.should eq(File.read("./spec/pages/page_#{attr}.xml"))
+      end
     end
   end
 end
