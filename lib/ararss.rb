ee84c79aa4ccbdfe3393aa51772a920b09f5d202
yous <yousbe@gmail.com>
2012-01-04 01:43:11 +0900

    article이 없을 두 가지 경우 테스트, 그에 따른 class 수정

--- lib/ararss.rb
+++ lib/ararss.rb
@@ -25,13 +25,15 @@ class ARA_RSS
       a = Mechanize.new
       resp = a.get "http://ara.kaist.ac.kr/board/Wanted/"
       resp.search('//table[@class="articleList"]/tbody/tr').each do |r|
-        m.items.new_item do |item|
-          title = (r.at('td[@class="title "]') or r.at('td[@class="title  deleted"]'))
-          item.title = title.inner_text.strip
-          item.date = Time.parse(r.at('td[@class="date"]').inner_html.strip)
-          item.guid.content = r.at('td[@class="articleid hidden"]').inner_text.strip
-          item.guid.isPermaLink = false
-          item.link = "http://ara.kaist.ac.kr/board/Wanted/#{item.guid.content}"
+        if r.inner_html.strip != ""
+          m.items.new_item do |item|
+            title = (r.at('td[@class="title "]') or r.at('td[@class="title  deleted"]'))
+            item.title = title.inner_text.strip
+            item.date = Time.parse(r.at('td[@class="date"]').inner_html.strip)
+            item.guid.content = r.at('td[@class="articleid hidden"]').inner_text.strip
+            item.guid.isPermaLink = false
+            item.link = "http://ara.kaist.ac.kr/board/Wanted/#{item.guid.content}"
+          end
         end
       end
     end
