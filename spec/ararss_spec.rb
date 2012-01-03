bff139241bb4b079563fe193584f61453f06f6e2
yous <yousbe@gmail.com>
2012-01-04 01:19:31 +0900

    세 개의 비슷한 테스트를 do end로 묶음

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -3,27 +3,13 @@ require "fakeweb"
 require "./lib/ararss"
 
 describe "ARA RSS" do
-  it "gets the page that contains deleted articles and parse" do
-    stream = File.read("./spec/pages/page_deleted.html")
-    FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
-    
-    rss = ARA_RSS.new
-    rss.data.should eq(File.read("./spec/pages/page_deleted.xml"))
-  end
+  ["deleted", "one", "one_deleted"].each do |attr|
+    it "gets the page that contains #{attr} article(s) and parse" do
+      stream = File.read("./spec/pages/page_#{attr}.html")
+      FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
 
-  it "gets the page that contains only one article and parse" do
-    stream = File.read("./spec/pages/page_one.html")
-    FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
-
-    rss = ARA_RSS.new
-    rss.data.should eq(File.read("./spec/pages/page_one.xml"))
-  end
-
-  it "gets the page that contains only one deleted article and parse" do
-    stream = File.read("./spec/pages/page_one_deleted.html")
-    FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
-
-    rss = ARA_RSS.new
-    rss.data.should eq(File.read("./spec/pages/page_one_deleted.xml"))
+      rss = ARA_RSS.new
+      rss.data.should eq(File.read("./spec/pages/page_#{attr}.xml"))
+    end
   end
 end
