a86dba26e9a3221d70504bb9a67443a899b522a5
yous <yousbe@gmail.com>
2012-01-03 23:15:07 +0900

    테스트 페이지들을 pages 폴더로 이동

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -8,10 +8,10 @@ end
 
 describe "ARA RSS" do
   it "gets the first page of ARA board and parse" do
-    stream = File.read("./spec/fake_ara_page.html")
+    stream = File.read("./spec/pages/fake_ara_page.html")
     FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
     
     rss = ARA_RSS.new
-    rss.data.should eq(File.read("./spec/fake_ara_page.xml"))
+    rss.data.should eq(File.read("./spec/pages/fake_ara_page.xml"))
   end
 end
