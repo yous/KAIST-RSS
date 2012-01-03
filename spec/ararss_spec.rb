7ae0621109f0d282a7c455128d4d644f3bd66f42
yous <yousbe@gmail.com>
2012-01-04 01:01:52 +0900

    article이 하나만 있고 deleted인 경우 테스트

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -18,4 +18,12 @@ describe "ARA RSS" do
     rss = ARA_RSS.new
     rss.data.should eq(File.read("./spec/pages/page_one.xml"))
   end
+
+  it "gets the page that contains only one deleted article and parse" do
+    stream = File.read("./spec/pages/page_one_deleted.html")
+    FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
+
+    rss = ARA_RSS.new
+    rss.data.should eq(File.read("./spec/pages/page_one_deleted.xml"))
+  end
 end
