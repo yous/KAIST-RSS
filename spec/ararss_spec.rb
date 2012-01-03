87c7167d91f7b0967bc384aa107ac3ce46ada241
yous <yousbe@gmail.com>
2012-01-04 02:09:25 +0900

    작성자 또는 제목에 특이한 문자가 포함됐을 때 테스트

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -3,7 +3,7 @@ require "fakeweb"
 require "./lib/ararss"
 
 describe "ARA RSS" do
-  ["deleted", "one", "one_deleted", "none", "none_2"].each do |attr|
+  ["deleted", "one", "one_deleted", "none", "none_2", "trouble"].each do |attr|
     it "gets the page that contains #{attr} article(s) and parse" do
       stream = File.read("./spec/pages/page_#{attr}.html")
       FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
