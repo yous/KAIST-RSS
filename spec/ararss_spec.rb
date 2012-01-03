aae6ac45787594485f3dbc3b68689f8717a53bef
yous <yousbe@gmail.com>
2012-01-04 00:55:00 +0900

    article이 하나만 있는 경우 테스트

--- spec/ararss_spec.rb
+++ spec/ararss_spec.rb
@@ -10,4 +10,12 @@ describe "ARA RSS" do
     rss = ARA_RSS.new
     rss.data.should eq(File.read("./spec/pages/page_deleted.xml"))
   end
+
+  it "gets the page that contains only one article and parse" do
+    stream = File.read("./spec/pages/page_one.html")
+    FakeWeb.register_uri(:get, "http://ara.kaist.ac.kr/board/Wanted/", :body => stream, :content_type => "text/html")
+
+    rss = ARA_RSS.new
+    rss.data.should eq(File.read("./spec/pages/page_one.xml"))
+  end
 end
