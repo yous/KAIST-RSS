2e8eca6a44a87f514495973931b6d50da1c876cd
yous <yousbe@gmail.com>
2012-01-04 03:07:29 +0900

    아라의 다른 보드에 대해서도 RSS를 만들 수 있도록 class 수정

--- lib/ararss.rb
+++ lib/ararss.rb
@@ -4,10 +4,12 @@ require "bundler/setup"
 require "mechanize"
 
 class ARA_RSS
-  attr_reader :rss
+  attr_reader :rss, :version, :board
 
-  def initialize(version = "2.0")
+  def initialize(board = "Wanted", version = "2.0")
+    @board = board
     @version = version
+    @url = (board == "all") ? board : "board/#{board}"
     get
   end
 
@@ -17,13 +19,13 @@ class ARA_RSS
 
   def get
     @rss = RSS::Maker.make(@version) do |m|
-      m.channel.title = "ARA Wanted RSS Feed"
-      m.channel.link = "http://ara.kaist.ac.kr/board/Wanted/"
-      m.channel.description = "News at ARA Wanted Board"
+      m.channel.title = (@board == "all") ? "ARA RSS Feed" : "ARA #{@board} RSS Feed"
+      m.channel.link = "http://ara.kaist.ac.kr/#{@url}/"
+      m.channel.description = (@board == "all") ? "News at ARA Board" : "News at ARA #{@board} Board"
       m.items.do_sort = true
 
       a = Mechanize.new
-      resp = a.get "http://ara.kaist.ac.kr/board/Wanted/"
+      resp = a.get "http://ara.kaist.ac.kr/#{@url}/"
       resp.search('//table[@class="articleList"]/tbody/tr').each do |r|
         if r.inner_html.strip != ""
           m.items.new_item do |item|
@@ -32,7 +34,7 @@ class ARA_RSS
             item.date = Time.parse(r.at('td[@class="date"]').inner_html.strip)
             item.guid.content = r.at('td[@class="articleid hidden"]').inner_text.strip
             item.guid.isPermaLink = false
-            item.link = "http://ara.kaist.ac.kr/board/Wanted/#{item.guid.content}"
+            item.link = "http://ara.kaist.ac.kr/#{@url}/#{item.guid.content}"
           end
         end
       end
