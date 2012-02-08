56c4eeafde60b1a68cb60795f7ca86a6ae8e116a
yous <yousbe@gmail.com>
2012-02-09 07:02:01 +0900

    mobile 유저 filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def mobile_user_agent?
+    agent = request.env["HTTP_USER_AGENT"]
+    agent and (agent[/mobile/i] or agent[/iphone/i] or agent[/android/i] or agent[/blackberry/i])
+  end
 end
