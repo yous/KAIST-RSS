078aa097a826a42aa4cab59e2ef27558c8d9048f
yous <yousbe@gmail.com>
2012-02-09 09:59:25 +0900

    service div를 helper 함수로 분리

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,28 @@
 # encoding: utf-8
 
 module HomeHelper
+  def service
+    menus = get_menu
+
+    cont1 = content_tag(:ul, :id => "menu") do
+      _cont1 = menus["link"].map do |menu|
+        link_to(get_title(menu["index"]), {:controller => "rss", :board => menu["url"]}, :class => "menu#{menu["index"]}", :target => "_blank")
+      end.reduce(:<<)
+      _cont2 = menus["category"].map do |menu|
+        content_tag(:li, get_title(menu["index"]), :id => "menu#{menu["index"]}")
+      end.reduce(:<<)
+      _cont1 + _cont2
+    end
+    cont2 = menus["category"].map do |menu|
+      content_tag(:div, :id => "submenu#{menu["index"]}", :class => "submenu") do
+        menu["submenu"].map do |submenu|
+          link_to(submenu["title"], {:controller => "rss", :board => submenu["url"]}, :target => "_blank") + tag("br")
+        end.reduce(:<<)
+      end
+    end.reduce(:<<)
+    cont1 << cont2
+  end
+
   def get_menu
     menus = {"link" => [], "category" => []}
     a = Mechanize.new
