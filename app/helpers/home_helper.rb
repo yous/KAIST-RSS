998060343494c35e8b930670b7ed669c32a3e8a9
yous <yousbe@gmail.com>
2012-02-10 21:47:23 +0900

    ARA RSS에서 Portal RSS로 변경

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,69 +1,27 @@
 # encoding: utf-8
+require "json"
 
 module HomeHelper
   def service
     menus = get_menu
 
     cont1 = content_tag(:ul, :id => "menu") do
-      _cont1 = menus["link"].map do |menu|
-        link_to(get_title(menu["index"]), {:controller => "rss", :board => menu["url"]}, :class => "menu#{menu["index"]}", :target => "_blank")
-      end.reduce(:<<)
-      _cont2 = menus["category"].map do |menu|
-        content_tag(:li, get_title(menu["index"]), :id => "menu#{menu["index"]}")
-      end.reduce(:<<)
-      _cont1 + _cont2
+      content_tag(:li, "Portal", :id => "menu1")
     end
-    cont2 = menus["category"].map do |menu|
-      content_tag(:div, :id => "submenu#{menu["index"]}", :class => "submenu") do
-        menu["submenu"].map do |submenu|
-          link_to(submenu["title"], {:controller => "rss", :board => submenu["url"]}, :target => "_blank") + tag("br")
-        end.reduce(:<<)
-      end
-    end.reduce(:<<)
+    cont2 = content_tag(:div, :id => "submenu1", :class => "submenu") do
+      menus.map do |menu|
+        link_to(menu["title"], {:controller => "rss", :board => menu["url"], :title => menu["title"]}, :target => "_blank") + tag("br")
+      end.reduce(:<<)
+    end
     cont1 << cont2
   end
 
   def get_menu
-    menus = {"link" => [], "category" => []}
     a = Mechanize.new
-    resp = a.get "http://ara.kaist.ac.kr/main/"
-    resp.search('//div[@id="navigation"]/ul[@class="menu"]/li/a').each do |r|
-      menu = Hash.new
-      if r.attr("id") == "menuFavorite"
-        menu["index"] = 0
-        menu["url"] = $1 if r.attr("href") =~ /\/([\w\W]*)\//
-        menus["link"].push(menu)
-      else
-        menu["index"] = $1.to_i if r.attr("id") =~ /menuCategory(\d+)/
-        menu["submenu"] = []
-        resp.search("//dl[@id=\"boardInCategory#{menu["index"]}\"]/dd/ul/li/a").each do |sub_r|
-          submenu = Hash.new
-          submenu["url"] = $1 if sub_r.attr("href") =~ /\/board\/([\w\W]*)\//
-            submenu["title"] = sub_r.inner_html.strip
-          menu["submenu"].push(submenu)
-        end
-        menus["category"].push(menu)
-      end
+    resp = JSON.parse(a.get("https://portal.kaist.ac.kr/api/notice/?format=json").body)
+    menus = resp.map do |board|
+      {"title" => board["name"], "url" => board["slug"]}
     end
     menus
-  end
-
-  def get_title(menu)
-    case menu
-    when 0
-      "모아보기"
-    when 1
-      "KAIST"
-    when 2
-      "TALK"
-    when 3
-      "SHARE"
-    when 4
-      "INFO"
-    when 5
-      "HOBBY"
-    when 6
-      "ARA"
-    end
   end
 end
--- app/views/rss/index.xml.builder
+++ app/views/rss/index.xml.builder
@@ -1,33 +1,24 @@
-board = params[:board] || "all"
-url = (board == "all") ? board : "board/#{board}"
+require "json"
+
+board = params[:board] or "notice"
+title = params[:title]
 
 xml.instruct!
 xml.rss "version" => "2.0", "xmlns:content" => "http://purl.org/rss/1.0/modules/content/", "xmlns:dc" => "http://purl.org/dc/elements/1.1/", "xmlns:trackback" => "http://madskills.com/public/xml/rss/module/trackback/", "xmlns:itunes" => "http://www.itunes.com/dtds/podcast-1.0.dtd" do
   xml.channel do
-    if board == "all"
-      xml.title "ARA RSS Feed"
-      xml.description "News at ARA Board"
-    else
-      xml.title "ARA #{board} RSS Feed"
-      xml.description "News at ARA #{board} Board"
-    end
-    xml.link "http://ara.kaist.ac.kr/#{url}/"
-
     a = Mechanize.new
-    (1..3).each do |page_no|
-      resp = a.get "http://ara.kaist.ac.kr/#{url}/?page_no=#{page_no}"
-      resp.search('//table[@class="articleList"]/tbody/tr').each do |r|
-        if r.inner_html.strip != ""
-          xml.item do
-            title = (r.at('./td[@class="title "]') or r.at('./td[@class="title  deleted"]'))
-            xml.title title.inner_text.strip.gsub("<", "＜").gsub(">", "＞")
-            time = Time.parse(r.at('./td[@class="date"]').inner_html.strip)
-            xml.pubDate time.strftime("%a, %d %b %Y %H:%M:%S %z")
-            guid = r.at('./td[@class="articleid hidden"]').inner_text.strip
-            xml.guid guid, :isPermaLink => false
-            xml.link "http://ara.kaist.ac.kr/#{url}/#{guid}"
-          end
-        end
+    resp = JSON.parse(a.get("https://portal.kaist.ac.kr/api/notice/#{board}/?format=json&page=1").body)
+
+    xml.title "KAIST Portal #{title}"
+    xml.description "KAIST Portal #{title} RSS"
+    xml.link "https://portal.kaist.ac.kr/notice/#{board}/"
+    resp.each do |article|
+      xml.item do
+        xml.title "#{article["title"]} - #{article["user"]["first_name"]} #{article["user"]["last_name"]}"
+        xml.pubDate Time.parse(article["created_at"]).to_s(:rfc822)
+        xml.link "https://portal.kaist.ac.kr/notice/#{board}/#{article["id"]}/"
+        xml.guid "https://portal.kaist.ac.kr/notice/#{board}/#{article["id"]}/"
+        xml.description article["content"]
       end
     end
   end
